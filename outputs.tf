ca5aef0f2a7ce4dfa6bd5b6b173cd67482e6caca
Rachel Sharp <rachel@hashicorp.com>
2021-04-21 15:44:22 -0400

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,8 @@
+output "instance_id" {
+  value = aws_instance.example.id
+}
+
+output "public_ip" {
+  value       = aws_instance.example.public_ip
+  description = "The public IP of the web server"
+}
