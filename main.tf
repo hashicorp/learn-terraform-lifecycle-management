0c455ec12a3083127f6b58c7ba9856c29e4b8392
Tu Nguyen <im2nguyen@gmail.com>
2022-02-25 09:51:10 -0800

    Update to address security vulnerability

--- main.tf
+++ main.tf
@@ -34,8 +34,11 @@ resource "aws_instance" "example" {
   vpc_security_group_ids = [aws_security_group.sg_web.id]
   user_data              = <<-EOF
               #!/bin/bash
-              echo "Hello, World" > index.html
-              nohup busybox httpd -f -p 8080 &
+              apt-get update
+              apt-get install -y apache2
+              sed -i -e 's/80/8080/' /etc/apache2/ports.conf
+              echo "Hello World" > /var/www/html/index.html
+              systemctl restart apache2
               EOF
   tags = {
     Name          = "terraform-learn-state-ec2"
@@ -49,6 +52,13 @@ resource "aws_security_group" "sg_web" {
     from_port   = "8080"
     to_port     = "8080"
     protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  // required to update apt-get
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
